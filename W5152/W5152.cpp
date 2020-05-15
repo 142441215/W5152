06fb803bef2a56672d117021f25d8b8e52451e84
严浩 <1424412158@qq.com>
2020-05-15 14:20:00 +0800

    2

--- W5152/W5152.cpp
+++ W5152/W5152.cpp
@@ -1,20 +1,52 @@
-﻿// W5152.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+int equalizeMain()
+{
+	cv::Mat srcMat = imread("C://project//etest.jpg", 0);
+	cv::Mat dstMat;
 
-#include <iostream>
+	if (srcMat.empty()) {
+		cout << "fail to read pic!" << endl;
+		return -1;
+	}
 
+	if (srcMat.type() == CV_8UC1) {
+		equalizeHist(srcMat, dstMat);
+	}
+	else if (srcMat.type() == CV_8UC3) {
+		Mat channel[3];
+		Mat out[3];
+		split(srcMat, channel);
+		for (int i = 0; i < 3; i++) {
+			equalizeHist(channel[i], out[i]);
+		}
+
+		merge(out, 3, dstMat);
+
+	}
+
+
+	float srcHist[256];
+	float dstHist[256];
+	cv::Mat srcHistMat;
+	cv::Mat dstHistMat;
+	imshow("srcMat", srcMat);
+	imshow("dstMat", dstMat);
+	waitKey(0);
+
+	destroyAllWindows();
+
+	return 0;
+
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+	double start = static_cast<double>(cvGetTickCount());
+	equalizeMain();
+	double time = ((double)cvGetTickCount() - start) / cvGetTickFrequency();
+	cout << "processing time:" << time / 1000 << "ms" << endl;
+	system("pause");
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
